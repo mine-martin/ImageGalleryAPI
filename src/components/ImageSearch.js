d17c4df92aff6ef8da038d6c9c1dc0921571ff5e
martin_mine <53045063+mine-martin@users.noreply.github.com>
2021-08-05 14:33:35 +0300

    Updates

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -15,7 +15,7 @@ const ImageSearch = ({ searchText }) => {
             onChange={(e) => setText(e.target.value)}
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            placeholder="Search Images eg ..cat.."
+            placeholder="Search Images eg ..cat..painting..sun.."
           />
           <button
             className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
